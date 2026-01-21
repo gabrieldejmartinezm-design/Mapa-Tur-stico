4821ef85cffa9cb8e1746380f79f16ee35f7e953
gabrieldejmartinezm-design <gabrieldejmartinezm@gmail.com>
2026-01-20 23:27:37 -0400

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,48 +1,37 @@
 var wms_layers = [];
 
 
-        var lyr_GoogleTerrain_0 = new ol.layer.Tile({
-            'title': 'Google Terrain',
+        var lyr_GoogleSatellite_0 = new ol.layer.Tile({
+            'title': 'Google Satellite',
             'opacity': 1.000000,
             
             
             source: new ol.source.XYZ({
             attributions: '&nbsp;&middot; <a href="https://www.google.at/permissions/geoguidelines/attr-guide.html">Map data ©2015 Google</a>',
-                url: 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}'
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
             })
         });
-
-        var lyr_ESRISatellite_1 = new ol.layer.Tile({
-            'title': 'ESRI Satellite',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-            attributions: ' ',
-                url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-            })
-        });
-var format_MapaTuristicoPNCanaima_2 = new ol.format.GeoJSON();
-var features_MapaTuristicoPNCanaima_2 = format_MapaTuristicoPNCanaima_2.readFeatures(json_MapaTuristicoPNCanaima_2, 
+var format_mapa_1 = new ol.format.GeoJSON();
+var features_mapa_1 = format_mapa_1.readFeatures(json_mapa_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_MapaTuristicoPNCanaima_2 = new ol.source.Vector({
+var jsonSource_mapa_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_MapaTuristicoPNCanaima_2.addFeatures(features_MapaTuristicoPNCanaima_2);
-var lyr_MapaTuristicoPNCanaima_2 = new ol.layer.Vector({
+jsonSource_mapa_1.addFeatures(features_mapa_1);
+var lyr_mapa_1 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_MapaTuristicoPNCanaima_2, 
-                style: style_MapaTuristicoPNCanaima_2,
-                popuplayertitle: 'Mapa Turistico PN Canaima',
+                source:jsonSource_mapa_1, 
+                style: style_mapa_1,
+                popuplayertitle: 'mapa',
                 interactive: true,
-                title: '<img src="styles/legend/MapaTuristicoPNCanaima_2.png" /> Mapa Turistico PN Canaima'
+                title: '<img src="styles/legend/mapa_1.png" /> mapa'
             });
 
-lyr_GoogleTerrain_0.setVisible(true);lyr_ESRISatellite_1.setVisible(true);lyr_MapaTuristicoPNCanaima_2.setVisible(true);
-var layersList = [lyr_GoogleTerrain_0,lyr_ESRISatellite_1,lyr_MapaTuristicoPNCanaima_2];
-lyr_MapaTuristicoPNCanaima_2.set('fieldAliases', {'Atractivo': 'Atractivo', 'Tipo': 'Tipo', 'Coordenada': 'Coordenada', 'UTM ( E)': 'UTM ( E)', 'UTM (N)': 'UTM (N)', 'Dirección': 'Dirección', 'Municipio': 'Municipio', 'Estado': 'Estado', 'Descripci�': 'Descripci�', 'Sector Ind': 'Sector Ind', 'Parque Nac': 'Parque Nac', 'Servicios': 'Servicios', 'Ente Regul': 'Ente Regul', 'Registro F': 'Registro F', });
-lyr_MapaTuristicoPNCanaima_2.set('fieldImages', {'Atractivo': 'TextEdit', 'Tipo': 'TextEdit', 'Coordenada': 'TextEdit', 'UTM ( E)': 'TextEdit', 'UTM (N)': 'TextEdit', 'Dirección': 'TextEdit', 'Municipio': 'TextEdit', 'Estado': 'TextEdit', 'Descripci�': 'TextEdit', 'Sector Ind': 'TextEdit', 'Parque Nac': 'TextEdit', 'Servicios': 'TextEdit', 'Ente Regul': 'TextEdit', 'Registro F': 'ExternalResource', });
-lyr_MapaTuristicoPNCanaima_2.set('fieldLabels', {'Atractivo': 'inline label - always visible', 'Tipo': 'inline label - always visible', 'Coordenada': 'inline label - always visible', 'UTM ( E)': 'inline label - always visible', 'UTM (N)': 'inline label - always visible', 'Dirección': 'inline label - always visible', 'Municipio': 'inline label - always visible', 'Estado': 'inline label - always visible', 'Descripci�': 'inline label - always visible', 'Sector Ind': 'inline label - always visible', 'Parque Nac': 'inline label - always visible', 'Servicios': 'inline label - visible with data', 'Ente Regul': 'inline label - visible with data', 'Registro F': 'inline label - visible with data', });
-lyr_MapaTuristicoPNCanaima_2.on('precompose', function(evt) {
+lyr_GoogleSatellite_0.setVisible(true);lyr_mapa_1.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_mapa_1];
+lyr_mapa_1.set('fieldAliases', {'Atractivo': 'Atractivo', 'Tipo': 'Tipo', 'Coordenada': 'Coordenada', 'UTM ( E)': 'UTM ( E)', 'UTM (N)': 'UTM (N)', 'Dirección': 'Dirección', 'Municipio': 'Municipio', 'Estado': 'Estado', 'Descripci�': 'Descripci�', 'Sector Ind': 'Sector Ind', 'Parque Nac': 'Parque Nac', 'Servicios': 'Servicios', 'Ente Regul': 'Ente Regul', 'Registro F': 'Registro F', });
+lyr_mapa_1.set('fieldImages', {'Atractivo': 'TextEdit', 'Tipo': 'TextEdit', 'Coordenada': 'TextEdit', 'UTM ( E)': 'TextEdit', 'UTM (N)': 'TextEdit', 'Dirección': 'TextEdit', 'Municipio': 'TextEdit', 'Estado': 'TextEdit', 'Descripci�': 'TextEdit', 'Sector Ind': 'TextEdit', 'Parque Nac': 'TextEdit', 'Servicios': 'TextEdit', 'Ente Regul': 'TextEdit', 'Registro F': 'ExternalResource', });
+lyr_mapa_1.set('fieldLabels', {'Atractivo': 'inline label - visible with data', 'Tipo': 'inline label - visible with data', 'Coordenada': 'inline label - visible with data', 'UTM ( E)': 'inline label - visible with data', 'UTM (N)': 'inline label - visible with data', 'Dirección': 'inline label - visible with data', 'Municipio': 'inline label - visible with data', 'Estado': 'inline label - visible with data', 'Descripci�': 'inline label - visible with data', 'Sector Ind': 'inline label - visible with data', 'Parque Nac': 'inline label - visible with data', 'Servicios': 'inline label - visible with data', 'Ente Regul': 'inline label - visible with data', 'Registro F': 'inline label - visible with data', });
+lyr_mapa_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
